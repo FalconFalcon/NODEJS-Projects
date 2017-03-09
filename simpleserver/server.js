13ce2384c54aafa3ad28bec7b23ff8faf7bc4598
Adithya KLN <adithyakarlapalem@gmail.com>
2017-03-08 16:33:11 -0800

    Simple Server and Express Setups

--- simpleserver/server.js
+++ simpleserver/server.js
@@ -1,28 +1,62 @@
-// const http = require('http');
-
-// const hostname = '127.0.0.1';
-// const port = 3000;
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World\n');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
 const http = require('http');
-const hostname = '127.0.0.1';
-const port = 1898;
 
-const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World\n');
-});
+const url = require('url'); //URL module allowing the URLs to be detected
+var path = require('path'); //Path module allowing the paths to be deteced
+const fs = require('fs'); //Filesystem module allowing to tfiles to be detected
 
-server.listen(port, hostname , () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
-})
+
+var mimeTypes = {
+//Array of Mime types
+	"html" : "text/html",
+	"jpeg" : "image/jpeg",
+	"jpg" : "image/jpg",
+	"png" :"image/png",
+	"js" : "text/javascript",
+	"css" : "text/css"
+
+};
+
+//Create server
+http.createServer(function(req, res){
+	var uri = url.parse(req.url).path;
+	var fileName = path.join(process.cwd(),unescape(uri));
+	console.log('Loading' +uri);
+	var stats;
+
+//check if it's a file access or a directory access
+ 
+	try{
+	stats = fs.lstatSync(fileName);
+	}
+	catch(e){
+	console.log(e);	
+	res.writeHead(404, {'Content-type': 'text/plain'});
+	res.write('404 Not Found');
+	res.end();
+	return;
+	}
+
+
+	if(stats.isFile()){
+	var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]]
+	res.writeHead(200, {'Content-type': mimeType});
+
+	var fileStream = fs.createReadStream(fileName);
+	fileStream.pipe(res);
+
+
+	} 
+
+	else if(stats.isDirectory()){
+	res.writeHead(302, {'Location' : 'index.html'});
+	res.end();
+
+	} else {
+	res.writeHead(500, {'Content-type' : 'text/plain'});
+	res.write('500 Internal Error\n');
+	res.end();
+	}
+}).listen(3000);
+
+
+
